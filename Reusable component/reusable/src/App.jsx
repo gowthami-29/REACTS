f1de68d854db57709e35e5feb9479a9c3ccc04f4
gowthami-29 <gowthamikanchi29@gmail.com>
2025-12-16 15:12:49 +0530

    props

---
+++ Reusable component/reusable/src/App.jsx
@@ -0,0 +1,12 @@
+import Reusable from "./MessageCard";
+
+function App(){
+return(<>
+  <Reusable title="Welcome" message="hello user"/>
+  <Reusable title="South Korea" message="I love south korea"/>
+  <Reusable title="Info" message="check your email"/>
+  <Reusable title="Error" message="Something went wrong"/>
+</>
+)
+}
+export default App;
---
+++ Reusable component/reusable/src/MessageCard.jsx
@@ -0,0 +1,10 @@
+function Reusable({title,message}){
+
+    return(
+        <div>
+            <h3>{title}</h3>
+            <p>{message}</p>
+        </div>
+    )
+}
+export default Reusable;
