825d75dc7989bb95be8cace2c68d63af8362cdcf
gowthami-29 <gowthamikanchi29@gmail.com>
2026-01-03 20:44:11 +0530

    evalutions

--- EVALUTION/evalution/src/pages/admin/AdminDashboard.jsx
+++ EVALUTION/evalution/src/pages/admin/AdminDashboard.jsx
@@ -1,4 +1,35 @@
-function AdminDashboard(){
+import { useEffect, useState } from "react";
+import { data, useNavigate } from "react-router-dom";
+import RestaurantCard from "./components/RestaurantCard"
+import AddRestaurantForm from "./components/AddRestaurantForm"
+function AdminRestaurant(){
+    const [restaurants,setRestaurants]=useState([]);
+    const [search,setSearch]=useState("");
+    const [type,setType]=useState("");
+    const [parking,setParking]=useState("");
+    const navigate=useNavigate();
+    useEffect(()=>{
+        setRestaurants(JSON.parse(localStorage.getItem("resta"))|| [])
+
+    },[])
+    return(
+        <div>
+            <Navbar search={search}
+            setSearch={setSearch}
+            setType={setType}
+            setParking={setParking}/>
+            <AddRestaurantForm addRestaurant={addRestaurant}/>
+            {
+                filtered.map((r)=>(<RestaurantCard 
+                    key={r.id}
+                    data={r}
+                    isAdmin={true}
+                    onDelete={handleDelete}
+                    onUpdate={()=>navigate("/admin/restaurants/update",{state:r})}/>
+                ))
+            }
+        </div>
+    )
 
 }
-export default AdminDashboard;
+export default AdminRestaurant;
--- EVALUTION/evalution/src/pages/admin/UpdateRestaurant.jsx
+++ EVALUTION/evalution/src/pages/admin/UpdateRestaurant.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import {useLocation,useNavigate} from "react-router-dom"
+
 function UpdateRestaurant(){
+    const {state}=useLocation();
+    const navigate=useNavigate();
+    const [form,setForm]=useState(state);
+    const handleUpdate=()=>{
+        const data=JSON.parse(localStorage.getItem("restaurant",JSON.stringify(updated)))
+        alert("restaurant updated")
+        navigate("/admin/dashboard");
+    }
+    return(
+        <div>
+            <input value={form.name} onChange={(e)=>setForm({...form,name:e.target.value})} />
+             <input value={form.address} onChange={(e)=>setForm({...form,address:e.target.value})} />
+             <button onClick={handleUpdate}>Update</button>
+        </div>
+    )
+
 
 }
 export default UpdateRestaurant;
